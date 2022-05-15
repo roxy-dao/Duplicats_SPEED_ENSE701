337e9224b6c733eb41db1adede08ed7cfebc6a28
roxy-codes <roxeh.cakes@gmail.com>
2022-05-15 15:39:30 +1200

    set up heroku

--- server/server.js
+++ server/server.js
@@ -1,17 +1,22 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
-const connectDb = require("./database/db");
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+const connectDb = require('./database/db')
+const app = express()
 
-connectDb();
+connectDb()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-app.use("/api/articles", require("./routes/articles"));
-app.use("/api/practices", require("./routes/practice"));
+app.use('/api/articles', require('./routes/articles'))
+app.use('/api/practices', require('./routes/practice'))
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}.`));
+app.use(express.static(path.resolve(__dirname, './frontend/build')))
+app.get('*', function (request, response) {
+  response.sendFile(path.resolve(__dirname, './frontend/build', 'index.html'))
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => console.log(`Server running on port ${PORT}.`))
