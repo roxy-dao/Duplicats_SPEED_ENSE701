8b2d5d3223daf0ad078fbd2beeadf42529663a03
roxy-codes <roxeh.cakes@gmail.com>
2022-05-29 12:56:39 +1200

    Mock data

---
+++ database/mockData.js
@@ -0,0 +1,25 @@
+module.exports.mockPractices = [{ practice: 'TDD' }, { practice: 'Mob Programming' }]
+
+module.exports.mockArticles = [
+  {
+    title:
+      'An experimental evaluation of test driven development vs. test-last development with industry professionals',
+    authors: 'Munir, H., Wnuk, K., Petersen, K., Moayyed, M.',
+    source: 'EASE',
+    pubyear: 2014,
+    doi: 'https://doi.org/10.1145/2601248.2601267',
+    claim: 'code quality improvement',
+    evidence: 'strong support',
+    sepractice: 'TDD',
+  },
+  {
+    title: 'Mob programming helps improve the performance of teams',
+    authors: 'John Doe',
+    source: 'IEEE Software',
+    pubyear: 2018,
+    doi: 'https://doi.org/10.1109/SW.2018.29',
+    claim: 'code quality improvement',
+    evidence: 'weak support',
+    sepractice: 'Mob Programming',
+  },
+]
