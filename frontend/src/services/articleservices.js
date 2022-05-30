51a80fac097d031d1464f8606a818544d9a57e57
roxy-codes <roxeh.cakes@gmail.com>
2022-05-30 16:33:02 +1200

    Update articleservices.js

--- frontend/src/services/articleservices.js
+++ frontend/src/services/articleservices.js
@@ -1,11 +1,11 @@
-import axios from "axios";
+import axios from 'axios'
 
 const getArticles = async () => {
-  return axios.get("/api/articles").then((articles) => articles.data);
-};
+  return axios.get('/api/articles').then((articles) => articles.data)
+}
 
 const postArticle = async (article) => {
-  return axios.post("/api/articles", article).then((articles) => articles.data);
-};
+  return axios.post('/api/articles', article).then((articles) => articles.data)
+}
 
-export { getArticles, postArticle };
+export { getArticles, postArticle }
